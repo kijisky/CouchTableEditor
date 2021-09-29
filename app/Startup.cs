6267c6dc41e6fbcf4ed5ea649d39c9b5fd97e46a
kiji <kiji@yandex.ru>
2021-09-29 22:39:19 +0300

    init app

--- app/Startup.cs
+++ app/Startup.cs
@@ -39,8 +39,9 @@ namespace app
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
+            
 
             app.UseRouting();
 
